58bdd8b512e16841ee4660370c2b2a33f8665d60
Daniel molina <programanime@gmail.com>
2018-11-16 20:40:05 -0500

    cambios base de datos

--- scripts/01-bd.sql
+++ scripts/01-bd.sql
@@ -1,12 +1,16 @@
 connect 'jdbc:derby://localhost:1527/matedu;user=root;password=root;create=true;';
-create table pago(
+create table usuario(
+    id int PRIMARY KEY  GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
+    email varchar(128) not null, 
+    nombre varchar(128) not null, 
+    password varchar(32) not null
+);
+
+create table material(
     id int PRIMARY KEY  GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
     nombre varchar(128) not null, 
-    email varchar(128) not null, 
-    tipoTarjeta varchar(32) not null, 
-    fechaVencimiento varchar(32) not null, 
-    transaccion varchar(256) not null, 
-    numeroTarjeta bigint not null, 
-    valorTransaccion bigint not null, 
-    codigoSeguridad varchar(3) not null
+    descripcion varchar(128) not null, 
+    autor varchar(32) not null, 
+    valor varchar(32) not null, 
+    rutaArchivo varchar(256) not null
 );
